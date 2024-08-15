806a7706daf8bf824b093b90577c4a967591a095
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2024-08-15 11:48:39 +0200

    Ändere GitHub Link zum APG Projekt

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -32,7 +32,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/julianbaffy/APG-coursepage">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
