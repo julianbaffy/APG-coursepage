cc1fa220a414ace3ad45d5e356eabeed7c319a0b
julianbaffy <142730946+julianbaffy@users.noreply.github.com>
2024-08-17 00:09:15 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,8 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: vitePreprocess(),
-
+export default {
   kit: {
-    // Use the Node adapter instead of the auto adapter
-    adapter: adapter({
-      out: 'build' // Optional: Customize the output directory
-    })
+    adapter: adapter(),
+    // Weitere Konfigurationen
   }
 };
-
-export default config;
